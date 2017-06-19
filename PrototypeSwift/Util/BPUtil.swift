56b663e7a1b8bdda85b525596fd89f7f72fe7c76
GeekRRK <669672615@qq.com>
2017-06-19 15:12:49 +0800

    Clean project

--- PrototypeSwift/Util/BPUtil.swift
+++ PrototypeSwift/Util/BPUtil.swift
@@ -18,35 +18,5 @@ class BPUtil: NSObject {
     class func localStr(_ str: String) -> String {
         return NSLocalizedString(str, comment: "")
     }
-    
-    /*
-    func colorWithHexString(string2Convert: String) -> UIColor {
-        var cString = string2Convert.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
-        if cString.characters.count < 6 {
-            return UIColor.white
-        }
-        
-        let index = cString.index(cString.startIndex, offsetBy: 0)
-        cString = cString.substring(from: index);
-        
-        if cString.hasPrefix("#") {
-            let index = cString.index(cString.startIndex, offsetBy: 1)
-            cString = cString.substring(from: cString.index(after: index))
-        }
-        if cString.characters.count != 6 {
-            return UIColor.white
-        }
-        
-        var range: NSRange
-        range.location = 0
-        range.length = 2
-        let rString = cString.substring(with: range)
-        range.location = 2
-        let gString = cString.substring(with: range)
-        range.location = 4
-        let bString = cString.substring(with: range)
-        
-        let r, g, b: UInt
-    }
-     */
+
 }
